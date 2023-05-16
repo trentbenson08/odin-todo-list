16e713cb7bb32323f04a10d0bf4d82f90c74ebcc
trent <122744132+trentbenson08@users.noreply.github.com>
2023-05-15 23:08:19 -0500

    Setup linter and module bundler

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
