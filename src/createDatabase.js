fbf53b8d9e4b2e1550b7cc49a32f9933dc2108ad
trent <122744132+trentbenson08@users.noreply.github.com>
2023-05-16 16:37:52 -0500

    Add createDatabase.js

---
+++ src/createDatabase.js
@@ -0,0 +1,23 @@
+export default function createDatabase(){
+  const database = [];
+
+  // Takes an object, pushes it to the database and returns the index where the item
+  const addItem = (todoItem) => {
+    const itemIndex = database.push(todoItem) - 1;
+    return itemIndex;
+  };
+
+
+  // Takes unique ID from data-index finds it in the database
+  const deleteItem = (index) => {
+    database.splice(index, 1);
+  };
+
+  const getDatabase = () => database;
+
+  return {
+    addItem,
+    deleteItem,
+    getDatabase
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import createDatabase from './createDatabase';
+
 import './styles/reset.css';
 import './styles/style.css';
+
+
+// eslint-disable-next-line no-unused-vars
+const db = createDatabase();
+
+
+// eslint-disable-next-line no-debugger
+debugger;
